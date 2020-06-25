35b438c8796f656c814eb91e328e04bb99b2063e
Nikolay Chernov <nchernov@gmail.com>
2020-06-25 23:37:11 +0300

    local vendors libs

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo
 
 from .models import User
 
@@ -13,7 +13,6 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(2, 30)])
-    email = StringField("Email Address", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(6, 30)])
     password_confirmation = PasswordField(
         "Confirm Password",
@@ -27,7 +26,3 @@ class RegistrationForm(FlaskForm):
     def validate_username(form, field):
         if User.query.filter_by(username=field.data).first() is not None:
             raise ValidationError("This username is taken.")
-
-    def validate_email(form, field):
-        if User.query.filter_by(email=field.data).first() is not None:
-            raise ValidationError("This email is already registered.")
