b9edcbb320b00ddd1503793c3a970294187b366d
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-28 12:09:13 +0300

    Add a logger, an exception handler

--- app/tab/utils.py
+++ app/tab/utils.py
@@ -1,4 +1,5 @@
 import json
+from app.logger import log
 from .models import Tab
 
 CONFIG_LOCATION = '/home/belenov/simple2b/Avantika/tabs.json'
@@ -10,9 +11,15 @@ def get_allowed_tabs(client):
 
 def load_tabs():
     tab_objects = []
-    with open(CONFIG_LOCATION, 'r') as f:
-        for tab in json.load(f):
-            tab_objects.append(Tab.from_dict(**tab))
+    try:
+        with open(CONFIG_LOCATION, 'r') as f:
+            for tab in json.load(f):
+                tab_objects.append(Tab.from_dict(**tab))
+    except OSError as ose:
+        log(log.ERROR, "OS error: {0}".format(ose))
+    except json.JSONDecodeError as jsde:
+        log(log.ERROR, "OS error: {0}".format(jsde))
+
     return tab_objects
 
 
