387b705b0fe6ff4e6b6bc58c25da0ee11d72ac2e
LikaDevelop <likakniazieva@gmail.com>
2020-07-08 16:29:04 +0300

    Delete and Edit User

--- app/static/main.js
+++ app/static/main.js
@@ -1,1 +1,21 @@
 // custom javascript
+$(document).ready(function () {
+
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(".user_delete_button").click(function (e) {
+
+        if (confirm('Do you want delete the user?')) {
+            // Save it!
+            console.log('Delete user aproved');
+        } else {
+            // Do nothing!
+            console.log('Delete user canceled');
+            e.preventDefault();
+        }
+    });
+
+});
