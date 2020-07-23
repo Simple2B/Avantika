26b8ce3981af7ab322ba8ef4e0b479319385eca6
LikaDevelop <likakniazieva@gmail.com>
2020-07-23 13:51:19 +0300

    status user

--- app/result/views.py
+++ app/result/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_user import current_user
 from app.tab import get_allowed_tabs
+from app.result.models import Result
 from app.exam.models import Exam
 
 result_blueprint = Blueprint("result", __name__)
@@ -8,8 +9,15 @@ result_blueprint = Blueprint("result", __name__)
 
 @result_blueprint.route("/status")
 def result():
+    # For output user name in the site's form
     user = current_user.username
-    # All exam for exam_level current user
-    exam = Exam.query.all()
+    user_id_results = Result.query.filter(Result.user_id == current_user.id).all()
+    exam_name = Exam.query.filter(Result.exam_id == Exam.id).all()
     tabs = get_allowed_tabs()
-    return render_template("exam/status.html", tabs=tabs, user=user, exam=exam)
+    return render_template(
+        "exam/status.html",
+        tabs=tabs,
+        user=user,
+        user_id_results=user_id_results,
+        exam_name=exam_name,
+    )
