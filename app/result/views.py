0ef04a50431916510570e007aa49a4be753296de
LikaDevelop <likakniazieva@gmail.com>
2020-07-29 12:40:21 +0300

    change pasword for all user

--- app/result/views.py
+++ app/result/views.py
@@ -2,12 +2,14 @@ from flask import Blueprint, render_template
 from flask_user import current_user
 from app.tab import get_allowed_tabs
 from app.result.models import Result
+from app.auth.models import User
 
 result_blueprint = Blueprint("result", __name__)
 
 
 @result_blueprint.route("/status")
 def result():
+    user = User.query.filter(User.id == current_user.id).first()
     results = Result.query.filter(Result.user_id == current_user.id).all()
     tabs = get_allowed_tabs()
-    return render_template("exam/status.html", tabs=tabs, results=results)
+    return render_template("exam/status.html", tabs=tabs, results=results, user=user)
